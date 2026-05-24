0dbe3125b41b2e9dc976ef3c05a36196201f82f7
kayisdagasli-cmd <kayisdag.asli@ogr.ahievran.edu.tr>
2026-05-24 11:34:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -176,4 +176,5 @@ def api_aylik_vaka():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
